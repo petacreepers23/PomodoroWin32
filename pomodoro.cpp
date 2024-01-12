66601eaebd65b1b18226397f6385098265bd0dd8
Miguel González <jmiguelgo@indra.es>
2024-01-12 15:17:20 +0100

    fix style, add vscode config

--- pomodoro.cpp
+++ pomodoro.cpp
@@ -3,40 +3,43 @@
 
 const char c_windowsClassName[] = "pomodoroClass";
 
-std::string g_texto;
-UINT_PTR g_timerId;
+UINT_PTR g_timer_id;
 HFONT g_hfont;
+bool g_timer_running = false;
+int g_seconds_left = 0;
 
-enum botones {BTN_Null, BTN_Focus, BTN_Short, BTN_Long, BTN_StartStop, BTN_NullEnd};
+enum botones 
+{
+    BTN_Null, BTN_Focus, BTN_Short, BTN_Long, BTN_StartStop, BTN_NullEnd
+};
 
-bool l_timer_running = false;
-int l_seconds_left = 0;
-
-void CalculateNewTimerText() {
-    int minutes = l_seconds_left / 60;
-    int seconds = l_seconds_left % 60;
-    g_texto = (minutes < 10 ? "0" : "") + std::to_string(minutes) + ":" + (seconds < 10 ? "0" : "") + std::to_string(seconds);
+std::string CalculateNewTimerText() 
+{
+    int minutes = g_seconds_left / 60;
+    int seconds = g_seconds_left % 60;
+    return (minutes < 10 ? "0" : "") + std::to_string(minutes) + ":" + (seconds < 10 ? "0" : "") + std::to_string(seconds);
 }
 
 void DoCountdownAndUpdateText(HWND hwnd, UINT, UINT_PTR, DWORD)
 {
-    if (!l_timer_running)
+    if (!g_timer_running)
     {
         return;
     }
 
     InvalidateRect(hwnd, NULL, FALSE);
     
-    if (l_seconds_left>0) 
+    if (g_seconds_left>0) 
     {
-        l_seconds_left--;
+        g_seconds_left--;
     }
 
     CalculateNewTimerText();
 
-    if (l_seconds_left == 0) {
-        l_timer_running = false;
-	Beep(440,2000);
+    if (g_seconds_left == 0) 
+    {
+        g_timer_running = false;
+	    Beep(440,2000);
     }
 }
 
@@ -95,19 +98,19 @@ void HandleMenuPress (HWND hwnd, int wParam)
     switch(wParam)
     {
         case BTN_Focus:
-            g_texto = "25:00";
-            l_seconds_left = 1500;
+            g_timer_running = false;
+            g_seconds_left = 1500;
             break;
         case BTN_Short:
-            g_texto = "5:00";
-            l_seconds_left = 300;
+            g_timer_running = false;
+            g_seconds_left = 300;
             break;
         case BTN_Long:
-            g_texto = "15:00";
-            l_seconds_left = 900;
+            g_timer_running = false;
+            g_seconds_left = 900;
             break;
         case BTN_StartStop:
-            l_timer_running = !l_timer_running;
+            g_timer_running = !g_timer_running;
             break;
         default:
             break;
@@ -130,11 +133,11 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
             DestroyWindow(hwnd);
             break;
         case WM_DESTROY:
-            KillTimer(hwnd,g_timerId);
+            KillTimer(hwnd,g_timer_id);
             PostQuitMessage(0);
             break;
         case WM_PAINT:
-            UpdateText(hwnd, g_texto.c_str());
+            UpdateText(hwnd, CalculateNewTimerText().c_str());
             break;
         default:
             return DefWindowProc(hwnd, msg, wParam, lParam);
@@ -171,9 +174,9 @@ void RegisterPomodoroClass (HINSTANCE hInstance)
 void DefinePeriodicTimer(HWND hwnd)
 {
     //This is unreliable, not real time, just that you know.
-    g_timerId = SetTimer(hwnd,0,1000,DoCountdownAndUpdateText);
+    g_timer_id = SetTimer(hwnd,0,1000,DoCountdownAndUpdateText);
 
-    if(!g_timerId)
+    if(!g_timer_id)
     {
         MessageBox(NULL, "Timer Creation Failed!", "Error!",
             MB_ICONEXCLAMATION | MB_OK);
@@ -230,7 +233,6 @@ void DefinePomodoroFont()
     logFont.lfWeight = FW_BOLD;
     strcpy(logFont.lfFaceName, "Broadway");
     g_hfont = CreateFontIndirect(&logFont);
-    g_texto = "0:00";
 }
 
 // Compile with -lgdi32
